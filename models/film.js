d117352a2428bd2056a8c8fe1a959fd18f5b820c
achasik <achasik@gmail.com>
2019-05-02 12:50:47 +0400

    Movie trailers

--- models/film.js
+++ models/film.js
@@ -1,44 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const FilmSchema = new Schema(
-   {
-      kp: {
-         type: Number,
-         unique: true,
-         sparse: true
-      },
-      tmdb: {
-         type: Number,
-         unique: true,
-         sparse: true
-      },
-      imdb: {
-         type: String,
-         unique: true,
-         sparse: true
-      },
-      name: String,
-      nameRu: String,
-      description: String,
-      year: {
-         type: Number,
-         required: true
-      },
-      poster: String,
-      seen: Boolean,
-      torrents: [
-         {
-            type: Schema.Types.ObjectId,
-            ref: 'torrent'
-         }
-      ]
-   },
-   {
-      timestamps: true,
-      usePushEach: true
-   }
+  {
+    kp: {
+      type: Number,
+      unique: true,
+      sparse: true
+    },
+    tmdb: {
+      type: Number,
+      unique: true,
+      sparse: true
+    },
+    imdb: {
+      type: String,
+      unique: true,
+      sparse: true
+    },
+    name: String,
+    nameRu: String,
+    description: String,
+    year: {
+      type: Number,
+      required: true
+    },
+    poster: String,
+    seen: Boolean,
+    torrents: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "torrent"
+      }
+    ],
+    videos: [
+      {
+        id: String,
+        name: String
+      }
+    ]
+  },
+  {
+    timestamps: true,
+    usePushEach: true
+  }
 );
 
-const Film = mongoose.model('film', FilmSchema);
+const Film = mongoose.model("film", FilmSchema);
 module.exports = Film;
--- tmdb.js
+++ tmdb.js
@@ -1,49 +1,60 @@
-const tmdbclient = require('themoviedbclient');
+const tmdbclient = require("themoviedbclient");
 const api = new tmdbclient(process.env.TMDB_KEY);
 //const Film = require('./models/film');
 
-module.exports ={
-    search: async (possible) => {
-        var json = null;
-        if (possible.tmdb){
-            json = await api.call(`/movie/${possible.tmdb}`);
-        }else if(possible.imdb){
-            json = await api.call(`/find/${possible.imdb}`, {external_source:'imdb_id'});
-        }else{
-            json =await api.call('/search/movie', {query: possible.name || possible.nameRu, year: possible.year});
-        }
-        const found = await jsonToFilm(json, possible);
-        if (found)
-            return found;
-        if (possible.kp)
-            return possible;
-        return null;
+module.exports = {
+  search: async possible => {
+    var json = null;
+    if (possible.tmdb) {
+      json = await api.call(`/movie/${possible.tmdb}`);
+    } else if (possible.imdb) {
+      json = await api.call(`/find/${possible.imdb}`, {
+        external_source: "imdb_id"
+      });
+    } else {
+      json = await api.call("/search/movie", {
+        query: possible.name || possible.nameRu,
+        year: possible.year
+      });
     }
+    const found = await jsonToFilm(json, possible);
+    if (found) return found;
+    if (possible.kp) return possible;
+    return null;
+  }
 };
 
-const jsonToFilm = async function (json, possible){
-    var movie = null;
-    if(json.movie_results && json.movie_results.length > 0){
-        movie = json.movie_results[0];
-    }
-    else if(json.results && json.results.length>0){
-        movie = json.results[0];
-    }
-    else if(json.id){
-        movie = json;
-    }
-    else{
-        return null;
-    }
-    possible.tmdb = movie.id;
-    possible.name = movie.original_title;
-    possible.poster = movie.poster_path;
-    const description = await getRu(movie.id);
-    possible.description = description || movie.overview;    
-    return possible;
+const jsonToFilm = async function(json, possible) {
+  var movie = null;
+  if (json.movie_results && json.movie_results.length > 0) {
+    movie = json.movie_results[0];
+  } else if (json.results && json.results.length > 0) {
+    movie = json.results[0];
+  } else if (json.id) {
+    movie = json;
+  } else {
+    return null;
+  }
+  possible.tmdb = movie.id;
+  possible.name = movie.original_title;
+  possible.poster = movie.poster_path;
+  const description = await getRu(movie.id);
+  possible.description = description || movie.overview;
+  possible.videos = await getVideos(id);
+  return possible;
 };
 
-const getRu = async function (id){
-    const json = await api.call(`/movie/${id}`, {language: 'ru'});
-    return json ? json.overview : null;
+const getRu = async function(id) {
+  const json = await api.call(`/movie/${id}`, { language: "ru" });
+  return json ? json.overview : null;
+};
+
+const getVideos = async function(id) {
+  const json = await api.call(`/movie/${id}/videos`);
+  return json & json.results
+    ? json.results.map(r => {
+        name: r.name;
+        id: r.key;
+      })
+    : null;
 };
